bdaa571c52db55948e3fea524c81054e076c57e1
daveimpala43-2 <david_jhernandez@outlook.com>
2020-05-08 20:27:00 -0500

    Se hizo la maqutacion

---
+++ index.php
@@ -0,0 +1,100 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <?php
+    require_once('layouts/head.php');
+    ?>
+
+    <title>inicio</title>
+  </head>
+  <body>
+
+  <!-- CARRUSEL -->
+  <div class="container carr">
+         <div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
+         <ol class="carousel-indicators">
+           <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
+           <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
+           <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
+         </ol>
+         <div class="carousel-inner">
+           <div class="carousel-item active">
+             <img src="img/carrusel/1.png" class="d-block w-100 carrusel" alt="...">
+             <div class="carousel-caption d-none d-md-block">
+               <h5>First slide label</h5>
+               <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+             </div>
+           </div>
+           <div class="carousel-item">
+             <img src="img/carrusel/2.png" class="d-block w-100 carrusel" alt="...">
+             <div class="carousel-caption d-none d-md-block">
+               <h5>Second slide label</h5>
+               <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+             </div>
+           </div>
+           <div class="carousel-item">
+             <img src="img/carrusel/3.png" class="d-block w-100 carrusel" alt="...">
+             <div class="carousel-caption d-none d-md-block">
+               <h5>Third slide label</h5>
+               <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+             </div>
+           </div>
+         </div>
+         <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
+           <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+           <span class="sr-only">Previous</span>
+         </a>
+         <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
+           <span class="carousel-control-next-icon" aria-hidden="true"></span>
+           <span class="sr-only">Next</span>
+         </a>
+        </div>
+</div>
+  <!-- FIN CARRUSEL -->
+
+<div class="contenedor">
+    <div class="item1 ">
+        <div class="row">
+            <div class="card" >
+              <div class="card-body">
+                <h5 class="card-title">Efectos del covid</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" class="btn botton">Ver mas</a>
+              </div>
+            </div>
+
+            <div class="card" >
+              <div class="card-body">
+                <h5 class="card-title">Efectos del covid</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" class="btn botton">Ver mas</a>
+              </div>
+            </div>
+
+            <div class="card" >
+              <div class="card-body">
+                <h5 class="card-title">Efectos del covid</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" class="btn botton">Ver mas</a>
+              </div>
+            </div>
+
+            <div class="card" >
+              <div class="card-body">
+                <h5 class="card-title">Efectos del covid</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" class="btn botton">Ver mas</a>
+              </div>
+            </div>
+        </div>
+    </div>
+    <div class="item2 ">PRODUCTOS </div>
+
+</div>
+
+<?php
+ require_once('layouts/footer.php');
+?>
+  </body>
+</html>
---
+++ layouts/head.php
@@ -0,0 +1,38 @@
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<link rel="icon" type="image/ico" href="img/Logo.ico">
+<!-- Bootstrap CSS -->
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+<link rel="stylesheet" href="css/style.css">
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <img class="img-fluid logo" src="img/Logo.png" alt="">
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link display-7" href="#">Inicio <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle display-7" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Productos
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="#">Vestimenta Medica</a>
+          <a class="dropdown-item" href="#">Equipo Medico</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#">Labroatorio</a>
+        </div>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link display-7" href="#">Sobre Nosotros</a>
+      </li>
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <button class="btn btn-outline-dark my-2 my-sm-0" type="submit">Registrar</button>
+    </form>
+  </div>
+</nav>
