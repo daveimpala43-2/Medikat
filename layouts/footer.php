4474d306ce75a3ed277aa80eb2fb744f68acfbc8
daveimpala43-2 <david_jhernandez@outlook.com>
2020-05-11 20:59:43 -0500

    Se agregaro el footer con grid

--- layouts/footer.php
+++ layouts/footer.php
@@ -7,9 +7,41 @@
 
     <div class="footer">
         <div class="cabeza">MEDIKAT </div>
-        <div class="cont1">Mapa</div>
-        <div class="cont2">Redes Sociales</div>
-        <div class="cont3">Contacto</div>
-        <div class="cont4">Logo</div>
-        <div class="cont5">Derechos reservados a Medikat</div>
+        <div class="cont1">
+          <div class="derecho">
+          </div>
+          <div class="icono">
+            <h7><a href="https://goo.gl/maps/JQgwrvhXeVTNbfFf6"> <ion-icon size="medium" name="location-outline"></ion-icon> Ubicacion   <br> <h7 class ="visible-ubi"> Dr José María Vertiz 1110, Vértiz Narvarte, Benito Juárez, 03600 Ciudad de México, CDMX</h7>  </a> </h7> 
+          </div>
+        
+        </div>
+        <div class="cont2">
+            <div class="derecho">
+              </div> <h7 class ="Redes-soli"> Redes Sociales </h7>
+            <div class="derecho">
+            </div>
+            <div class="face">
+              <ion-icon name="logo-facebook">  </ion-icon> <h7>Facebook</h7>
+            </div>
+            <div class="derecho">
+            </div>
+            <div class="face">
+             <ion-icon name="logo-twitter"></ion-icon> <h7>Twitter</h7>
+            </div>
+        </div>
+        <div class="cont3">
+          <div class="derecho">
+          </div>Contacto
+          <div class="derecho">
+          </div>
+          <div class="phone"> <ion-icon name="call-outline"></ion-icon> 55-4324-3454</div>
+          <div class="derecho">
+          </div>
+          <div class="phone"> <ion-icon name="mail-outline"></ion-icon> contac@medikat.com </div>
+          </div>
+        <div class="cont4">
+          <div class="derecho">
+          </div> <img class="img-fluid logo" src="img/Logo.png">
+          </div>
+        <div class="cont5">Derechos reservados a Medikat ®</div>
     </div>
--- layouts/head.php
+++ layouts/head.php
@@ -3,6 +3,7 @@
 <link rel="icon" type="image/ico" href="img/Logo.ico">
 <!-- Bootstrap CSS -->
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+<script src="https://unpkg.com/ionicons@5.0.0/dist/ionicons.js"></script>
 <link rel="stylesheet" href="css/style.css">
 
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
